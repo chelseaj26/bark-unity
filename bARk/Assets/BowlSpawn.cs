882f395ba51c65a515e395d59164b5b7843b69a1
Chelsea <cej48@drexel.edu>
2021-02-17 15:36:11 -0500

    bowl toggle

---
+++ bARk/Assets/BowlSpawn.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BowlSpawn : MonoBehaviour
+{
+     public GameObject bowl;
+     public void Update() {
+        bowl.setActive(false); //deactivates bowl on load 
+     }
+    
+    public void AddBowl (GameObject bowl) {
+         bowl.setActive(true); //activates bowl on click
+       }
+    }
+
